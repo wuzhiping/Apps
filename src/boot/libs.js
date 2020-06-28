78a02455efbddfdc08319f8d6310375665bd7a33
shawoo <wu.zhiping@feg.cn>
2020-06-28 12:52:09 +0800

    socket & pouchDB

--- src/boot/libs.js
+++ src/boot/libs.js
@@ -5,6 +5,9 @@ import 'iview/dist/styles/iview.css'
 import VueQrcodeReader from 'vue-qrcode-reader'
 import formCreate from '@form-create/iview'
 
+import PouchDB from 'pouchdb';
+import io from 'socket.io-client/dist/socket.io.min'
+
 Vue.use(formCreate)
 Vue.use(VueQrcodeReader)
 Vue.use(iView)
